b39626b49cfbd7696aefc6d260d7e516c2381073
Krish Bhuva <bhuvakrish03@gmail.com>
2025-02-03 14:37:46 +0530

    DSA question

---
+++ 53.Medium.js
@@ -0,0 +1,21 @@
+//====================================O(n^3)====================================
+
+// let arr = [1]
+// let result = 0
+// let max = -Infinity
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     for(let j = i ; j < arr.length ; j++){
+//         let sum = 0 
+//         for(let k = i ; k <= j ; k++){
+//             sum += arr[k]
+//             max = Math.max(max,sum)
+//         }
+//     }
+// }
+
+// console.log(max);
+
+
+
+//====================================more optimized =============================
