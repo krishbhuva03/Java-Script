bec5e95a6a2f190af8d5ab83dd9cd731b6b91df6
Krish Bhuva <bhuvakrish03@gmail.com>
2025-02-11 00:00:16 +0530

    another dsa question

--- 53.Medium.js
+++ 53.Medium.js
@@ -19,3 +19,36 @@
 
 
 //====================================more optimized =============================
+
+// let arr = [5,4,-1,7,8]
+// let result = 0
+// let max = -Infinity
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     let sum = 0 
+//     for(let j = i ; j < arr.length ; j++){
+//         sum += arr[j]
+//         max = Math.max(max,sum)
+//     }
+// }
+
+// console.log(max);
+
+
+//====================================O(n)====================================
+
+
+let arr = [-2,1,-3,4,-1,2,1,-5,4]
+let result = 0
+let max = -Infinity
+
+for(let i = 0 ; i < arr.length ; i++){
+    for(let j = i ; j < arr.length ; j++){
+        let sum = 0 
+        for(let k = i ; k <= j ; k++){
+            sum += arr[k]
+            max = Math.max(max,sum)
+        }
+    }
+}
+return max;
