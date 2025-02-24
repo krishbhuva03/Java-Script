a24593f05a6b69a233f7f6041ffad86138228a36
Krish Bhuva <bhuvakrish03@gmail.com>
2025-02-24 23:32:22 +0530

    different question for string reversal

--- reversethe string.js
+++ reversethe string.js
@@ -1,13 +1,24 @@
-let string = "krish bhuva"
+// let string = "krish bhuva"
 
-function reverse (str){
-    let newString = ""
-    for(let i = str.length - 1 ; i >= 0 ; i--){
-        newString += str[i]
-    }
-    return newString
+// function reverse (str){
+//     let newString = ""
+//     for(let i = str.length - 1 ; i >= 0 ; i--){
+//         newString += str[i]
+//     }
+//     return newString
+// }
+
+// console.log(reverse(string));
+
+let string = "          my    name    is    krish                    bhuva    "
+string = string.trim().split(" ").filter(word => word !== "");
+console.log(string); 
+
+
+let newString = ""
+
+for(let i = string.length - 1 ; i >= 0 ; i--){
+    newString += string[i] + " "
 }
-
-console.log(reverse(string));
-
-
+newString = newString.trim()
+console.log(newString);
